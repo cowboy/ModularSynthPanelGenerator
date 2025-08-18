72b41b04171c891b72b5bb537496b9b7528498b2
Ben Alman <cowboy@rj3.net>
2025-08-17 23:38:02 -0400

    Create release action

---
+++ .github/build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+# set -o xtrace
+
+VERSION=$(jq --raw-output '.release.tag_name' "$GITHUB_EVENT_PATH")
+
+sed -i "s/(in-development)/$VERSION/" EurorackPanelGenerator.manifest
